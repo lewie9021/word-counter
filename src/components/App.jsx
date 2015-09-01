aca1708ea4f20ceba9a1ca1fd9c724aa04d74b8a
Lewis Barnes <lb.barnes@live.co.uk>
2015-09-01 18:32:14 +0100

    Added an onChange listener to the textarea. Created a simple input process function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Navbar, Grid, Row, Col, Panel, ListGroup, ListGroupItem, Badge } from "react-bootstrap";
 
-class App extends React.Component {
+class App extends Component {
 
     renderTextarea() {
         return (
             <Col xs={12} md={8} mdOffset={2}>
-                <textarea className="form-control" rows="10" placeholder="Enter text here..."></textarea>
+                <textarea
+                      className="form-control"
+                      rows="10"
+                      placeholder="Enter text here..."
+                      onChange={this.onTextAreaChange.bind(this)} />
             </Col>
         );
+    }
+
+    onTextAreaChange(e) {
+        var counts = this.processInput(e.target.value);
+        
+        console.log("Input counts:", counts);
+    }
+
+    processInput(input) {
+        var match;
+        var spacesAndWords = /(\s+)|(\w+)/g;
+        var counts = {
+            spaces: 0,
+            words: 0,
+            characters: input.length
+        };
+        
+        while (match = spacesAndWords.exec(input)) {
+            var space = (match[1] || "");
+            var word = (match[2] || "");
+
+            counts.spaces += space.length;
+
+            if (word.length)
+                counts.words += 1;
+        }
+
+        return counts;
     }
     
     renderBuckets() {
