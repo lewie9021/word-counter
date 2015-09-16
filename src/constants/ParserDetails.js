4c06241ecbd565f8a4763a5f52d0b928bdf9e5b6
Lewis Barnes <lb.barnes@live.co.uk>
2015-09-16 22:05:52 +0100

    Added a constants dir. Added an object map for the parser details map. We need to know what the text should be for the details key charactersNoSpaces for example.

---
+++ src/constants/ParserDetails.js
@@ -0,0 +1,9 @@
+const PARSER_DETAILS = {
+    words: "Words",
+    characters: "Characters",
+    charactersNoSpaces: "Characters (no spaces)",
+    sentences: "Sentences",
+    paragraphs: "Paragraphs"
+};
+
+export default PARSER_DETAILS;
